728e9a0ed30a0469b2d864d7b7ab0b22363b1327
welitonhs <weliton.sernajotto23@gmail.com>
2019-12-12 16:49:47 -0300

    Remove sqlite3 and pg for tests, now mysql run test to

--- app/Models/Token.js
+++ app/Models/Token.js
@@ -1,16 +1,13 @@
 'use strict'
 
-const { addHours, isBefore, parseISO } = require('date-fns')
+const { addHours, isBefore } = require('date-fns')
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Token extends Model {
   expired () {
-    const createdAt = typeof (this.created_at) !== 'string'
-      ? parseISO(this.created_at.toJSON()) : parseISO(this.created_at)
-
-    const expirationDateTimeToken = addHours(createdAt, 24)
+    const expirationDateTimeToken = addHours(this.created_at, 24)
 
     if (!isBefore(new Date(), expirationDateTimeToken)) {
       return true
